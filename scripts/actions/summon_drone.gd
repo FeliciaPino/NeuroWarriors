0da0aa4f2ff546586c51540b14aab62a29b3356a
Felicia <faguin2022@udec.cl>
2025-01-31 16:32:00 -0300

    Made it so that the swarm controller spawns varied drones

--- scripts/actions/summon_drone.gd
+++ scripts/actions/summon_drone.gd
@@ -1,6 +1,8 @@
 extends BattleAction
 # Called when the node enters the scene tree for the first time.
-var drone_scene = preload("res://scenes/characters/enemies/drone.tscn")
+const drone_scene = preload("res://scenes/characters/enemies/drone.tscn")
+const offensive_drone_scene = preload("res://scenes/characters/enemies/offensive_drone.tscn")
+const defensive_drone_scene = preload("res://scenes/characters/enemies/defensive_drone.tscn")
 func _ready() -> void:
 	super._ready()
 	action_name = "Summon drone"
@@ -16,4 +18,12 @@ func execute (user:BattleEntity, target:BattleEntity):
 	super.execute(user,target)
 	_ranged_non_projectile_action(user,target)
 func _action_effect(user:BattleEntity, target:BattleEntity)->void:
-	user.game_manager.entity_manager.spawn_entity(drone_scene.instantiate())
+	var random_number = randi()%100
+	var new_guy:BattleEntity
+	if random_number<= 40:
+		new_guy = drone_scene.instantiate()
+	elif random_number <= 70:
+		new_guy = offensive_drone_scene.instantiate()
+	else:
+		new_guy = defensive_drone_scene.instantiate()
+	user.game_manager.entity_manager.spawn_entity(new_guy)
