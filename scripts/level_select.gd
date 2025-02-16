ee3f8bfddf63279eab45b73b24373894a5f7e1a5
Felicia <feliciapino100@gmail.com>
2025-02-16 14:38:12 -0300

    Made main menu scene preload

--- scripts/level_select.gd
+++ scripts/level_select.gd
@@ -1,7 +1,8 @@
 extends Control
 @onready var return_button = $return
 @export var last_level:String
+const main_menu_scene = preload("res://scenes/main_menu.tscn")
 func _ready() -> void:
-	return_button.pressed.connect(func(): get_tree().change_scene_to_file("res://scenes/main_menu.tscn"))
+	return_button.pressed.connect(func(): get_tree().change_scene_to_packed(main_menu_scene))
 func load_level(path:String):
 	get_tree().change_scene_to_file(path)
