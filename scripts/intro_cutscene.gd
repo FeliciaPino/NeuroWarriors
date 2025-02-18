a8e05c16f5c8d7c43138b7720e4df7d171ce21dc
Felicia <feliciapino100@gmail.com>
2025-02-18 14:45:20 -0300

    Made cutscene get text from translations and also made it so the player can navigate it freely

--- scripts/intro_cutscene.gd
+++ scripts/intro_cutscene.gd
@@ -1,27 +1,55 @@
 extends Control
-@onready var frames = $frames
-@onready var animation_player  = $AnimationPlayer
-@onready var skip_button = $skip_button
-@onready var skip_button_timer = $skip_button_timer
-@onready var button_animation_player = $buttonAnimation
+@onready var frames_node = $frames
+@onready var skip_button = %SkipButton
+@onready var next_button = %NextButton
+@onready var previous_button = %PreviousButton
 
+var current_frame_index = 0
+@onready var fade:Sprite2D = $Fade
+@onready var label = %Label
+var frames:Array[Sprite2D] = []
 const level_select_scene = preload("res://scenes/levels/level_select.tscn")
 func _ready() -> void:
 	GameState.flags["watched_intro_cutscene"] = true
-	for child in frames.get_children():
+	for child in frames_node.get_children():
+		frames.append(child)
 		child.visible = false
+	frames[current_frame_index].visible = true
 	skip_button.pressed.connect(return_to_level_select)
-	skip_button_timer.timeout.connect(func(): button_animation_player.play_backwards("appear_button"))
+	next_button.pressed.connect(next_frame)
+	previous_button.pressed.connect(previous_frame)
 	print("playing intro cutscene")
-	animation_player.play("intro_cutscene")
+	#animation_player.play("intro_cutscene")
+func next_frame():
+	current_frame_index += 1
+	if current_frame_index >= frames.size()-1:
+		return_to_level_select()
+		return
+		
+	await get_tree().create_tween().tween_property(fade,"modulate",Color(0,0,0,1),0.7).finished
+	frames[current_frame_index-1].visible = false
+	frames[current_frame_index].visible = true
+	label.text = tr(str("CUTSCENE_FRAME_",current_frame_index))
+	await get_tree().create_timer(0.2).timeout
+	await get_tree().create_tween().tween_property(fade,"modulate",Color(0,0,0,0),0.7).finished
 	
+func previous_frame():
+	current_frame_index -= 1
+	if current_frame_index < 0:
+		return
+		
+	await get_tree().create_tween().tween_property(fade,"modulate",Color(0,0,0,1),0.7).finished
+	frames[current_frame_index+1].visible = false
+	frames[current_frame_index].visible = true
+	label.text = tr(str("CUTSCENE_FRAME_",current_frame_index))
+	await get_tree().create_timer(0.2).timeout
+	await get_tree().create_tween().tween_property(fade,"modulate",Color(0,0,0,0),0.7).finished
+
 func return_to_level_select():
 	get_tree().change_scene_to_packed(level_select_scene)
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.pressed and skip_button_timer.is_stopped():
-		print("show button")
-		button_animation_player.play("appear_button")
-		skip_button_timer.start()
+	pass
+		
 var frame_descriptions = [
 	"Cutscene: (imposing building with a large v on the side, in a modern city at night)\n Vedal AI building, 202X",
 	"Cutscene: (inside the buildings factory, Evil can be seen on a monitor, controlling robot arms to build a drone, another monitor reads 'swarm control' program)\n Unbeknownst to Vedal, Evil is working on her own project using the building's facilities",
