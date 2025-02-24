8aba082ef635c4e470b0e312df35cb66413bf4c5
Felicia <faguin2022@udec.cl>
2025-02-23 21:00:38 -0300

    Increased overworld enemy vision area, chasing time, and speed. Decreased overworld player speed.

--- scripts/overworld/overworld_character.gd
+++ scripts/overworld/overworld_character.gd
@@ -1,14 +1,18 @@
 extends CharacterBody2D
 class_name OverworldCharacter
 
-@export var camera_target:Node2D
+@export var camera:Camera2D
+@export var speed = 200
 
 @onready var sprite = $AnimatedSprite2D
 
-var speed = 200
 
 func _ready() -> void:
 	global_position = GameState.get_player_map_position()
+	camera.global_position = global_position
+	await get_tree().process_frame
+	camera.position_smoothing_enabled = true
+	
 func _physics_process(delta: float) -> void:
 	GameState.set_player_map_position(global_position)
 	var direction  = Input.get_vector("left","right","up","down")
@@ -21,5 +25,3 @@ func _physics_process(delta: float) -> void:
 		sprite.play("default")
 	move_and_slide()
 	
-	if camera_target:
-		camera_target.position = velocity/4
