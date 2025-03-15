654cc2fe3af80f2d7996f164c324f169738dfb32
Felicia <faguin2022@udec.cl>
2025-03-15 17:29:51 -0300

    Made Overclock into a status effect based ability

--- scripts/actions/overclock.gd
+++ scripts/actions/overclock.gd
@@ -2,6 +2,7 @@ extends BattleAction
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	super._ready()
+	effect_duration = 2
 	action_name = tr("BATTLE_ACTION_OVERCLOCK_NAME")
 	description = tr("BATTLE_ACTION_OVERCLOCK_DESCRIPTION").format({stat=tr("BATTLESTAT_AP_REGEN")})
 	verb = tr("BATTLE_ACTION_OVERCLOCK_VERB")
@@ -15,6 +16,9 @@ func _ready() -> void:
 func execute (user:BattleEntity, target:BattleEntity):
 	super.execute(user,target)
 	_ranged_non_projectile_action()
+const overclocked_effect = preload("res://scenes/status_effects/overclocked.tscn")
 func _action_effect()->void:
-	target.reduce_health(1)
-	target.update_ap(target.ap + 2)
+	var effect:StatusEffect = overclocked_effect.instantiate()
+	effect.turns_remaining = effect_duration
+	effect.intensity = 2
+	target.add_effect(effect)
---
+++ scripts/effects/overclocked.gd
@@ -0,0 +1,17 @@
+extends StatusEffect
+func _init() -> void:
+	effect_name = tr("STATUS_EFFECT_OVERCLOCKED_NAME")
+func get_description():
+		return tr("STATUS_EFFECT_OVERCLOCKED_DESCRIPTION").format({stat=tr("BATTLESTAT_AP_REGEN") ,amount=intensity,amount2=intensity*7})
+
+func start_effect():
+	super.start_effect()
+	affected.speed += intensity
+	
+func start_of_turn():
+	super.start_of_turn()
+	affected.reduce_health(intensity*5)
+
+func end_effect():
+	super.end_effect()
+	affected.speed -= intensity
