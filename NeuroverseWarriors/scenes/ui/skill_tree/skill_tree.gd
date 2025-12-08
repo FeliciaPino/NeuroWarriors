84f6a3957674554a691628e6939fc621796b12c3
Felicia <faguin2022@udec.cl>
2025-12-07 21:48:48 -0300

    Added limits to the camera on the skill tree, so you don't get lost

--- NeuroverseWarriors/scenes/ui/skill_tree/skill_tree.gd
+++ NeuroverseWarriors/scenes/ui/skill_tree/skill_tree.gd
@@ -2,7 +2,7 @@ extends Control
 class_name SkillTree
 @export var associated_character:String
 @export var connection_line:TextureRect
-@onready var camera = $Camera2D
+@onready var camera := $Camera2D
 func _ready() -> void:
 	get_viewport().gui_focus_changed.connect(func(_node):follow_focus=true)
 	$debugton3.grab_focus()
@@ -12,10 +12,12 @@ func _process(_delta):
 	var focused := get_viewport().gui_get_focus_owner()
 	if follow_focus and focused:
 		camera.global_position = lerp(camera.global_position,focused.global_position+focused.size*0.5,0.01)
-	
 	camera.position += camera_momentum
 	camera_momentum *= 0.9
 	if camera_momentum.length() < 0.1: camera_momentum = Vector2.ZERO
+	camera.position.x = clamp(camera.position.x,camera.limit_left + camera.get_viewport_rect().size.x/2, camera.limit_right + camera.get_viewport_rect().size.x/2)
+	camera.position.y = clamp(camera.position.y, camera.limit_top + camera.get_viewport_rect().size.y/2, camera.limit_bottom + camera.get_viewport_rect().size.y/2)
+	
 	$CanvasLayer/Tungesten_counter.update_displayed_value()
 	$debugton.text = str("press to get 6 moola (u haz ",GameState.get_tungesten_amount(),"moola)")
 	$debugton2.text = str("press to level up, (",associated_character," is lvl ",GameState.characters_save_info[associated_character]["level"]," rn")
